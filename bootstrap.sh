c8453327e8a23336833c722ede9e8c6163b83de5
Joshua Wright <jwright@hasborg.com>
2023-01-03 19:54:24 -0500

    Update bootstrap to use current zlib (zlib-1.2.12.zip is removed because of CVE-2022-37434)

--- bootstrap.sh
+++ bootstrap.sh
@@ -45,7 +45,7 @@ export PKG_CONFIG_PATH=${OUTDIR}/lib/pkgconfig
 
 OPENSSL_VERSION="1.0.2-chacha"
 LIBEVENT_VERSION="2.1.8-stable"
-ZLIB_VERSION="zlib-1.2.12"
+ZLIB_VERSION="zlib-1.2.13"
 
 FILE="${BUILDDIR}/downloads/${OPENSSL_VERSION}.zip"
 if [ ! -f $FILE ]; then
